ee41d5e3d008da6d3d52680a35e99583a53ae701
Shreyaans Nahata <63207667+IAmOZRules@users.noreply.github.com>
2021-03-25 12:14:22 +0530

    Delete Activity02 directory

--- Activity02/src/Package1/LockDownActivities.java
+++
@@ -1,8 +0,0 @@
-package Package1;
-
-public interface LockDownActivities {
-    public abstract void Yoga();
-    public abstract void Pilates();
-    public abstract void Meditation();
-    public abstract void Sleep();
-}
--- Activity02/src/Package1/LockDownActivities1.java
+++
@@ -1,8 +0,0 @@
-package Package1;
-
-public abstract class LockDownActivities1 {
-    public abstract void Chess();
-    public abstract void PingPong();
-    public abstract void Binge();
-    public abstract void Browse();
-}
